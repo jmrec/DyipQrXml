76e9a227538ed09153d4413dd1ba74c3881bca3e
Juan Miguel Recondo <juanmiguel.recondo@gmail.com>
2025-10-14 21:08:52 +0800

    build: add CameraX, Navigation, and Lifecycle dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,6 +33,9 @@ android {
 	kotlinOptions {
 		jvmTarget = "11"
 	}
+	buildFeatures {
+		viewBinding = true
+	}
 }
 
 dependencies {
@@ -42,6 +45,15 @@ dependencies {
 	implementation(libs.material)
 	implementation(libs.androidx.activity)
 	implementation(libs.androidx.constraintlayout)
+	implementation(libs.androidx.lifecycle.runtime.ktx)
+	implementation(libs.androidx.navigation.fragment.ktx)
+	implementation(libs.androidx.navigation.ui.ktx)
+	implementation(libs.androidx.camera.core)
+	implementation(libs.androidx.camera.camera2)
+	implementation(libs.androidx.camera.lifecycle)
+	implementation(libs.androidx.camera.view)
+	implementation(libs.androidx.lifecycle.viewmodel.ktx)
+	implementation(libs.androidx.navigation.runtime.ktx)
 	testImplementation(libs.junit)
 	androidTestImplementation(libs.androidx.junit)
 	androidTestImplementation(libs.androidx.espresso.core)
